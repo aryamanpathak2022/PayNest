fc003a96ba75cba958ec7d0114a1fb29829d9c1c
aryamanpathak2022 <Aryaman.Pathak@iiitb.ac.in>
2024-06-29 18:17:56 +0530

    Update

---
+++ my-turborepo/apps/user_app/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { Button } from "@repo/ui/button";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className="w-screen h-screen bg-green-400">
+      hello
+      
+    </div>
+  );
+}
